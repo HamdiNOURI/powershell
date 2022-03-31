a0729d65956ad29ddeb937e622fa1c3658da0c5c
hamdi.nouri@esprit.tn <hamdi.nouri@esprit.tn>
2022-03-31 16:12:16 +0100

    COMMIT CERTIFICATE

--- FINAL-CERT-OK-Remote.ps1
+++ FINAL-CERT-OK-Remote.ps1
@@ -2,53 +2,57 @@ write-host  "###################################################################
 write-host  "##############	   DEVELOPPED SCRIPT TO Manage certificate    ##############"
 write-host  "############################################################################"
 
+
+######################################################### FOR LOCAL  EXEC
+$global:BuildNum = $args[0]
+$global:CN = $args[1]  									#####Should not be Empty
+$global:FreindlyName = $args[2]    				#####Should not be Empty
+$global:DNS = $args[3] 									#####Should not be Empty
+$global:KeyPass = $args[4]  							#####Should not be Empty
+
+
+
+######################################################### FOR LOCAL  EXEC
+# $global:BuildNum = $ENV:BUILD_NUMBER
+# $global:CN = $env:CN     									#####Should not be Empty
+# $global:FreindlyName = $env:FreindlyName    				#####Should not be Empty
+# $global:DNS = $env:DNS  									#####Should not be Empty
+# $global:KeyPass = $env:Password  							#####Should not be Empty
+
+
 Invoke-Command -ComputerName Super-serv -ScriptBlock {
 ###############################################
-############ BLOC DE VARIABLES 
+############ BLOC DE VARIABLES LOCAL
 ###############################################
 $MyDate=Get-Date -Format "yyyyMMdd"
-# write-host  "$MyDate"
 $DirPath='F:\ProjCert'   														### To Be Updated with the Good Path
 $TempPath='F:\ProjCert\Template'   												### To Be Updated with the Good Path
 ####################################################################################################################
-
-
-# $CN = Read-Host -Prompt 'Input The CN '   										#####Should not be Empty
-# $FreindlyName = Read-Host -Prompt 'Input The Freindly Name '   					#####Should not be Empty
-# $DNS = Read-Host -Prompt 'Input The DNS '   									#####Should not be Empty
-# $KeyPass = Read-Host -Prompt 'Input The Password '   							#####Should not be Empty
-# $SAN=True|False  																### Menue deroulant
-
-$CN = $env:CN     									#####Should not be Empty
-$FreindlyName = $env:FreindlyName    				#####Should not be Empty
-$DNS = $env:DNS  									#####Should not be Empty
-$KeyPass = $env:Password  							#####Should not be Empty
-# $SAN=True|False  																### Menue deroulant
 
 
 
 ###############################################
 ############ CREATION DE REPERTOIR
 ###############################################
-New-Item -Path "$DirPath\$MyDate-$ENV:BUILD_NUMBER" -ItemType Directory
+New-Item -Path "$DirPath\$MyDate-$Using:BuildNum" -ItemType Directory
+
 
 ###############################################
 ############ COPIE DE LA TEMPLATE DANS LE REP
 ###############################################
-Copy-Item -Path "$TempPath\template_req.inf" -Destination "$DirPath\$MyDate-$ENV:BUILD_NUMBER\req.inf"
+Copy-Item -Path "$TempPath\template_req.inf" -Destination "$DirPath\$MyDate-$Using:BuildNum\req.inf"
 
 ###############################################
 ############ EDITER LA TEMPLATE DANS LE REP
 ###############################################
-#
 $CNToModify = 6
 $ColumnCN = 14
-$File="$DirPath\$MyDate-$ENV:BUILD_NUMBER\req.inf"
+$File="$DirPath\$MyDate-$Using:BuildNum\req.inf"
 (
   Get-Content $File | ForEach-Object {
     $Line = $_.ReadCount
     if($Line -eq $CNToModify) {
-      $_.Insert($ColumnCN, "$CN") } 
+      $_.Insert($ColumnCN, "$Using:CN") } 
     else {
       $_
     }
@@ -62,31 +66,41 @@ $ColumnFN = 16
   Get-Content $File | ForEach-Object {
     $Line = $_.ReadCount
     if($Line -eq $FNToModify) {
-      $_.Insert($ColumnFN, "$FreindlyName") } 
+      $_.Insert($ColumnFN, "$Using:FreindlyName") } 
     else {
       $_
     }
   } 
 ) | Set-Content $File
-###############################
+
 try 
 { 
-	Do{
-		$length = $DNS.length
-		$montest = $DNS.LastIndexOf("%")
-		# write-host "$montest"
-		$var = $DNS.substring($length -($length-$DNS.LastIndexOf("%"))+1)
-		# write-host "$var"
-		$DNS = $DNS.substring(0, $DNS.LastIndexOf("%"))
-		$TotalVar = "_continue_ = `"$var`""
-		write-host "$TotalVar"
-		ADD-content -path $File -value "$TotalVar"
-		# Add-Content -Value "c:\Users\Administrator\Desktop\`"foobar.exe`"" 
+$DNSTemp = $Using:DNS
+
+Do{
+	if (($DNSTemp.ToCharArray()) -contains '%'){
+		write-host "existing separation"
+	} else {
+		write-host "pas de separation"
+		ADD-content -path $File -value "_continue_ = `"$DNSTemp`""
+	}
+	$length = $DNSTemp.length
+	$montest = $DNSTemp.LastIndexOf("%")
+	write-host "separation trouvé : $montest"
+	$var = $DNSTemp.substring($length -($length-$DNSTemp.LastIndexOf("%"))+1)
+	write-host "ma variable $var"
+	$DNSTemp = $DNSTemp.substring(0, $DNSTemp.LastIndexOf("%"))
+	$TotalVar = "_continue_ = `"$var`&`""
+	write-host "Ma variable total est $TotalVar"
+	ADD-content -path $File -value "$TotalVar"
 	}Until($montest -eq -1)
+
 }
 catch  
 	{
 
-		write-host " te7cha "
+		write-host "The Key Pass is $Using:KeyPass "
 	}
 }
+	
+
